f4f606d4681ba176641ff14bf84cc9ea8fcd008f
gliv <livanos.g@gmail.com>
2023-12-23 13:40:23 +0200

    add strategy

--- main.py
+++ main.py
@@ -8,47 +8,54 @@ from config import IMAGES_DIR, BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, EPOCHS, INPUT_
 from local_utils import inspect_video
 from metrics import get_auc
 
-train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
-    directory=IMAGES_DIR,
-    labels="inferred",
-    label_mode="binary",
-    class_names=["normal", "anomaly"],
-    color_mode="rgb",
-    batch_size=BATCH_SIZE,
-    image_size=(IMG_HEIGHT, IMG_WIDTH),
-    # shuffle=True,
-    seed=1,
-    validation_split=0.2,
-    subset="both",
-    crop_to_aspect_ratio=True
-)
+if tf.config.list_physical_devices('GPU'):
+    strategy = tf.distribute.MirroredStrategy()
+else:
+    strategy = tf.distribute.get_strategy()
 
-class_names = train_ds.class_names
-num_classes = len(class_names)
-train_ds = train_ds.cache().shuffle(train_ds.cardinality()).prefetch(buffer_size=tf.data.AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
+global_batch_size = (BATCH_SIZE * strategy.num_replicas_in_sync)
+with strategy.scope():
 
-model = Sequential([
-    layers.Rescaling(1. / 255, input_shape=INPUT_SHAPE),
-    layers.SeparableConv2D(16, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.SeparableConv2D(32, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.SeparableConv2D(64, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(1, "sigmoid"),
-])
+    train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+        directory=IMAGES_DIR,
+        labels="inferred",
+        label_mode="binary",
+        class_names=["normal", "anomaly"],
+        color_mode="rgb",
+        batch_size=global_batch_size,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        # shuffle=True,
+        seed=1,
+        validation_split=0.2,
+        subset="both",
+        crop_to_aspect_ratio=True
+    )
+    class_names = train_ds.class_names
+    num_classes = len(class_names)
+    train_ds = train_ds.cache().shuffle(train_ds.cardinality()).prefetch(buffer_size=tf.data.AUTOTUNE)
+    val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-loss = keras.losses.BinaryFocalCrossentropy()
+    model = Sequential([
+        layers.Rescaling(1. / 255, input_shape=INPUT_SHAPE),
+        layers.SeparableConv2D(16, 3, depth_multiplier=5, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.SeparableConv2D(32, 3, depth_multiplier=5, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.SeparableConv2D(64, 3, depth_multiplier=5, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1, "sigmoid"),
+    ])
 
-model.compile(optimizer='adam',
-              loss=loss,
-              metrics=['accuracy'])
+    loss = keras.losses.BinaryFocalCrossentropy()
 
-history = model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS)
-model.save('model.keras')
+    model.compile(optimizer='adam',
+                  loss=loss,
+                  metrics=['accuracy'])
+
+    history = model.fit(train_ds, validation_data=val_ds, epochs=EPOCHS)
+    model.save('model.keras')
 
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
