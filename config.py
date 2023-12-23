78162beded8e3422bc525b2a6f6c85f4e7cbfd4f
gliv <livanos.g@gmail.com>
2023-12-23 13:09:37 +0200

    move hyperparams to config.py

--- config.py
+++ config.py
@@ -1,26 +1,36 @@
+import itertools
 import os
 from datetime import datetime
+
+URL_DATA_FILE = "https://repository.detectionnow.com/content/rgb/denim_elastane.mp4"
+FILE_NAME = os.path.basename(URL_DATA_FILE)
 
 SEED = None
 TRIAL_ID = str(datetime.now().strftime("%Y%m%d%H%M%S"))
 ROOT_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-CSV_FILE = os.path.join(DATA_DIR, "data.csv")
-RAW_DATA_DIR = os.path.join(DATA_DIR, "raw")
-PROC_DATA_DIR = os.path.join(DATA_DIR, "proc")
-PROC_IMAGES_DIR = os.path.join(PROC_DATA_DIR, "processed_images")
-IMAGES_DIR = os.path.join(PROC_DATA_DIR, "images")
 MODELS_DIR = os.path.join(ROOT_DIR, "models")
-URL_DATA_FILE = "https://repository.detectionnow.com/content/rgb/denim_elastane.mp4"
-FILE_NAME = os.path.basename(URL_DATA_FILE)
-VIDEO_PATH = os.path.join(RAW_DATA_DIR, os.path.basename(URL_DATA_FILE))
-TML_MODEL_FILE = os.path.join(MODELS_DIR, "isolation_forest", TRIAL_ID, "isolation_forest.model")
-DL_MODEL_FILE = os.path.join(MODELS_DIR, "autoencoder", TRIAL_ID, "autoencoder_model.keras")
-[os.makedirs(dirs) for dirs in [DATA_DIR, RAW_DATA_DIR, PROC_DATA_DIR, PROC_IMAGES_DIR, IMAGES_DIR] if
- not os.path.exists(dirs)]
-ABNORMAL_INDICES = [range(45, 538),
-                    range(799, 1266),
-                    range(1452, 2311),
-                    range(2495, 2931),
-                    range(2954, 3417),
-                    range(4092, 4817)]
+RAW_DIR = os.path.join(DATA_DIR, "raw")
+
+
+IMAGES_DIR = os.path.join(DATA_DIR, "images")
+ANOMALY_DIR = os.path.join(IMAGES_DIR, "anomaly")
+NORMAL_DIR = os.path.join(IMAGES_DIR, "normal")
+PROC_DIR = os.path.join(DATA_DIR, "processed_images")
+
+[os.makedirs(dirs, exist_ok=True) for dirs in
+ [DATA_DIR, MODELS_DIR, RAW_DIR, IMAGES_DIR, ANOMALY_DIR, NORMAL_DIR, PROC_DIR]]
+
+CSV_FILE = os.path.join(DATA_DIR, "data.csv")
+VIDEO_FILE = os.path.join(RAW_DIR, os.path.basename(URL_DATA_FILE))
+
+ANOMALY_INDICES = list(itertools.chain(range(45, 538), range(799, 1266), range(1452, 2311),
+                                       range(2495, 2931), range(2954, 3417), range(4092, 4817)))
+
+
+BATCH_SIZE = 32
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+CHANNELS = 3
+INPUT_SHAPE = (IMG_HEIGHT, IMG_WIDTH, CHANNELS)
+EPOCHS = 10
