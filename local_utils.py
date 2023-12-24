283b4236cb0ae4a1dd54933dcb7c913ee5acd8ed
livanosg <livanos.g@gmail.com>
2023-12-24 17:51:06 +0200

    add metrics

--- local_utils.py
+++ local_utils.py
@@ -1,36 +1,46 @@
 import cv2
 import keras.models
 import numpy as np
-
 from config import IMG_WIDTH, IMG_HEIGHT, VIDEO_FILE
 
 
 def inspect_video(video_path, model, threshold=0.5):
-    paused = True
-    cap = cv2.VideoCapture(video_path)
+    paused = False
+    all_ds = keras.utils.image_dataset_from_directory(
+        directory="data/all_data",
+        label_mode=None,
+        batch_size=1,
+        image_size=(IMG_HEIGHT, IMG_WIDTH),
+        shuffle=False,
+        crop_to_aspect_ratio=True
+    )
     if isinstance(model, str):
         model = keras.models.load_model(model, compile=False)
-    while True:
-        frame_index = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
-        ret, frame = cap.read()
+    frame_index = 0
+    for input_image in all_ds:
+        frame = np.squeeze(np.round(input_image, 0), axis=0).astype(np.uint8)
+        print(frame)
+        # while True:
+        # frame_index = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
         # todo color_code train and test set images
         window_title = f"Frame Index: {int(frame_index)} - {'Paused' if paused else 'Playing'}\t"
         window_name = "Video Window"
         text = ""
-        if not ret:
-            print("Error: Failed to read frame.")
-            break
+        # if not ret:
+        #     print("Error: Failed to read frame.")
+        #     break
         if paused:
             window_title = " ".join([window_title, "(Paused)"])
             text = "".join([text, "(Paused)"])
             color = (255, 0, 0)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         else:
             if not model:
                 text = "Inspection".join(text)
                 color = (255, 0, 0)
             else:
-                input_image = np.expand_dims(cv2.resize(frame, (IMG_WIDTH, IMG_HEIGHT)), axis=0).astype(float)
+                # input_image = np.expand_dims(cv2.resize(frame, (IMG_WIDTH, IMG_HEIGHT)), axis=0).astype(float)
                 y_probability = model.predict(input_image, verbose=0)
                 y_prediction = np.where(y_probability > threshold, 1, 0)
 
@@ -43,6 +53,8 @@ def inspect_video(video_path, model, threshold=0.5):
                 else:
                     text = "Normal"
                     color = (0, 255, 0)
+            frame_index += 1
+
         cv2.namedWindow(window_name, cv2.WINDOW_KEEPRATIO)
         cv2.setWindowTitle(window_name, window_title)
         cv2.putText(frame, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
@@ -53,23 +65,23 @@ def inspect_video(video_path, model, threshold=0.5):
         # Move back by 5 frames on 'b' key press
         elif key == ord('b'):
             frame_index = max(0, frame_index - 10)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         # Move back by 1 frame on 'v' key press
         elif key == ord('v'):
             frame_index = max(0, frame_index - 1)
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         # Move forward by 10 frames on 'f' key press
         elif key == ord('f'):
             frame_index += 10
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         # Move forward by 1 frame on 'd' key press
         elif key == ord('d'):
             frame_index += 1
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+            # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
         # Quit if 'q' key is pressed
         elif key == ord('q'):
             break
-    cap.release()
+    # cap.release()
     cv2.destroyAllWindows()
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import keras.losses
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 from keras import Sequential, layers
 from matplotlib import pyplot as plt
@@ -8,14 +9,9 @@ from sklearn.metrics import classification_report, roc_curve, auc, precision_rec
 from config import IMAGES_DIR, BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, EPOCHS, INPUT_SHAPE, VIDEO_FILE, SEED, LEARNING_RATE
 from local_utils import inspect_video
 
-if tf.config.list_physical_devices('GPU'):
-    strategy = tf.distribute.MirroredStrategy()
-else:
-    strategy = tf.distribute.get_strategy()
 
-global_batch_size = (BATCH_SIZE * strategy.num_replicas_in_sync)
-with strategy.scope():
-    train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+def train_val_ds(global_batch_size):
+    return tf.keras.utils.image_dataset_from_directory(
         directory=IMAGES_DIR,
         labels="inferred",
         label_mode="binary",
@@ -23,90 +19,102 @@ with strategy.scope():
         color_mode="rgb",
         batch_size=global_batch_size,
         image_size=(IMG_HEIGHT, IMG_WIDTH),
-        shuffle=True,
-        seed=SEED,
+        shuffle=False,
         validation_split=0.2,
         subset="both",
-        crop_to_aspect_ratio=True,
+        crop_to_aspect_ratio=False
     )
 
-    class_names = train_ds.class_names
-    num_classes = len(class_names)
-    train_ds = train_ds.cache().shuffle(train_ds.cardinality()).prefetch(buffer_size=tf.data.AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-    model = Sequential([
-        layers.Rescaling(1. / 255, input_shape=INPUT_SHAPE),
-        layers.SeparableConv2D(16, 3, depth_multiplier=5, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.SeparableConv2D(32, 3, depth_multiplier=5, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.SeparableConv2D(64, 3, depth_multiplier=5, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(1, "sigmoid"),
-    ])
+def train_model():
+    if tf.config.list_physical_devices('GPU'):
+        strategy = tf.distribute.MirroredStrategy()
+    else:
+        strategy = tf.distribute.get_strategy()
 
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE),
-                  loss=keras.losses.BinaryCrossentropy(),
-                  metrics=[keras.metrics.BinaryAccuracy(),
-                           keras.metrics.BinaryIoU()]
-                  )
+    global_batch_size = (BATCH_SIZE * strategy.num_replicas_in_sync)
+    with strategy.scope():
+        train_ds, val_ds = train_val_ds(global_batch_size)
 
-    history = model.fit(train_ds,
-                        validation_data=val_ds,
-                        epochs=EPOCHS,
-                        callbacks=[keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True),
-                                   keras.callbacks.ReduceLROnPlateau(patience=10, cooldown=5)])
+        train_ds = train_ds.cache().shuffle(train_ds.cardinality()).prefetch(buffer_size=tf.data.AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=tf.data.AUTOTUNE)
 
-model.save("model.keras", save_format="keras")
+        model = Sequential([
+            layers.Rescaling(1. / 255, input_shape=INPUT_SHAPE),
+            layers.SeparableConv2D(16, 3, depth_multiplier=5, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.SeparableConv2D(32, 3, depth_multiplier=5, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.SeparableConv2D(64, 3, depth_multiplier=5, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(1, "sigmoid"),
+        ])
 
-acc = history.history['binary_accuracy']
-val_acc = history.history['val_binary_accuracy']
+        model.compile(optimizer=keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+                      loss=keras.losses.BinaryCrossentropy(),
+                      metrics=[keras.metrics.BinaryAccuracy(),
+                               keras.metrics.BinaryIoU()]
+                      )
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs_range = range(1, len(val_acc) + 1)
-
-plt.figure(figsize=(9, 9))
-plt.subplot(2, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(2, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-
-val_ds = val_ds.unbatch()
-x_input = val_ds.map(lambda x, y: tf.expand_dims(x, axis=0))
-y_true = np.array([sample.numpy() for sample in val_ds.map(lambda x, y: y)])
-y_pred = model.predict(x_input)
+        history = model.fit(train_ds,
+                            validation_data=val_ds,
+                            epochs=EPOCHS,
+                            callbacks=[keras.callbacks.EarlyStopping(patience=20, restore_best_weights=True),
+                                       keras.callbacks.ReduceLROnPlateau(patience=10, cooldown=5),
+                                       keras.callbacks.ModelCheckpoint("model.keras", save_best_only=True)])
+        df = pd.DataFrame.from_dict(history.history)
+        df.to_csv("history.csv")
+        return model
 
 
-def print_curves(y_true, y_pred, thresh="roc"):
-    fpr, tpr, roc_thresholds = roc_curve(y_true=y_true, y_score=y_pred)
-    roc_threshold_idx = np.argmin(np.sqrt(np.power(fpr - 0, 2) + np.power(tpr - 1, 2)))
+def metrics(model, val_dataset, threshold_type="pr"):
+    df = pd.read_csv("history.csv")
+    acc = df['binary_accuracy']
+    val_acc = df['val_binary_accuracy']
+
+    loss = df['loss']
+    val_loss = df['val_loss']
+    epochs_range = range(len(df))
+
+    plt.figure(figsize=(9, 9))
+    plt.subplot(2, 2, 1)
+    plt.plot(epochs_range, acc, label='Training Accuracy')
+    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 2, 2)
+    plt.plot(epochs_range, loss, label='Training Loss')
+    plt.plot(epochs_range, val_loss, label='Validation Loss')
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    input_data = val_dataset.map(lambda x, y: x)
+    y_true = np.asarray([label for label in val_dataset.map(lambda x, y: y).unbatch().as_numpy_iterator()]).astype(int)
+
+    y_probability = model.predict(input_data)
+    fpr, tpr, roc_thresholds = roc_curve(y_true=y_true, y_score=y_probability)
+    roc_threshold_idx = np.argmin(np.sqrt(np.power(fpr, 2) + np.power(tpr - 1, 2)))
     roc_threshold = roc_thresholds[roc_threshold_idx]
     roc_auc = auc(fpr, tpr)
 
-    prec, rec, pr_thresholds = precision_recall_curve(y_true=y_true, probas_pred=y_pred)
-    pr_threshold_idx = np.argmin(np.sqrt(np.power(1 - prec - 0, 2) + np.power(rec - 1, 2)))
+    precision, recall, pr_thresholds = precision_recall_curve(y_true=y_true, probas_pred=y_probability)
+    pr_threshold_idx = np.argmax(np.sqrt(np.power(precision, 2) + np.power(recall, 2)))
     pr_threshold = pr_thresholds[pr_threshold_idx]
-    ap = average_precision_score(y_true=y_true, y_score=y_pred)
+    ap = average_precision_score(y_true=y_true, y_score=y_probability)
 
-    if thresh == "roc":
+    if threshold_type == "roc":
         threshold = roc_threshold
-    elif thresh == "pr":
+    elif threshold_type == "pr":
         threshold = pr_threshold
     else:
-        raise ValueError(f"Unknown threshold value: '{thresh}'. Please select between 'roc' and 'pr'")
+        raise ValueError(f"Unknown threshold value: '{threshold_type}'. Please select between 'roc' and 'pr'")
+
+    y_prediction = np.where(y_probability >= threshold, 1, 0)
 
     np.save("threshold.npy", threshold)
+
     # Plot the ROC curve
     plt.subplot(2, 2, 3)
     plt.plot(fpr, tpr, color='darkorange', lw=2,
@@ -114,7 +122,6 @@ def print_curves(y_true, y_pred, thresh="roc"):
     plt.plot([0, fpr[-1]], [0, tpr[-1]], color='navy', lw=2, linestyle='--')
     plt.plot(fpr[roc_threshold_idx], tpr[roc_threshold_idx], c='green', marker='o',
              label=f'ROC threshold = {np.round(roc_threshold, 4)}')
-
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic (ROC) Curve')
@@ -122,26 +129,26 @@ def print_curves(y_true, y_pred, thresh="roc"):
 
     # Plot the ROC curve
     plt.subplot(2, 2, 4)
-    plt.plot(1 - prec, rec, color='darkorange', lw=2,
+    plt.plot(recall, precision, color='darkorange', lw=2,
              label=f'PR curve (Average Precision = {np.round(ap, 4)})')
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.plot(1 - prec[pr_threshold_idx], rec[pr_threshold_idx], c='red', marker='o',
+    plt.plot(recall[pr_threshold_idx], precision[pr_threshold_idx] , c='red', marker='o',
              label=f'PR threshold = {np.round(pr_threshold, 4)}')
 
-    plt.xlabel('Precision')
-    plt.ylabel('Recall')
+    plt.ylabel('Precision')
+    plt.xlabel('Recall')
     plt.title('Precision-Recall Curve')
     plt.legend(loc='lower right')
     plt.show()
 
-    # Convert probabilities to binary predictions
-    estimated_class = np.where(y_pred > threshold, 1, 0).astype(int)
-
     # Print the classification report
-    print(classification_report(y_true, estimated_class, labels=(0, 1), target_names=class_names))
+    print(classification_report(y_true, y_prediction, labels=(0, 1), target_names=["normal", "abnormall"]))
     return threshold
 
 
-threshold = print_curves(y_true, y_pred)
-
-inspect_video(VIDEO_FILE, model, threshold=threshold)
+if __name__ == '__main__':
+    # model = train_model()
+    _, val_ds = train_val_ds(100)
+    # threshold = metrics(model, val_ds)
+    model = keras.models.load_model("model.keras")
+    threshold = metrics(model, val_ds)
+    inspect_video(VIDEO_FILE, model, threshold=threshold)
