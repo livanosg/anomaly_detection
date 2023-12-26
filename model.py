8d57820b9f8e62a5b6f22ac982d9b36e5253d43f
livanosg <livanos.g@gmail.com>
2023-12-27 01:47:52 +0200

    fix model.py

--- model.py
+++ model.py
@@ -1,23 +1,28 @@
 import numpy as np
 from keras import Sequential, layers
+from sklearn.metrics import precision_recall_curve
 
-from hyper_parameters import INPUT_SHAPE
-
-model = Sequential([
-    layers.Rescaling(1. / 255, input_shape=INPUT_SHAPE),
-    layers.SeparableConv2D(16, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.SeparableConv2D(32, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.SeparableConv2D(64, 3, depth_multiplier=5, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(1, "sigmoid"),
-])
+from config import INPUT_SHAPE
+from dataset import get_dataset_predictions
 
 
-def classifier(model, x_input, threshold):
-    y_output = model.predict(x_input)
-    y_pred = np.greater_equal(y_output, threshold).astype(int)
-    return y_pred
+def get_model():
+    return Sequential([
+        layers.SeparableConv2D(8, 3, depth_multiplier=3, padding='same', activation='relu', input_shape=INPUT_SHAPE),
+        layers.MaxPooling2D(),
+        layers.SeparableConv2D(16, 7, depth_multiplier=5, padding='same', activation='relu'),
+        layers.MaxPooling2D(),
+        layers.SeparableConv2D(32, 9, depth_multiplier=7, padding='same', activation='relu'),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(2, "softmax"),
+    ])
+
+
+def calculate_threshold(y_true, probas_pred):
+    precision, recall, thresholds = precision_recall_curve(y_true=y_true, probas_pred=probas_pred)
+    eucl_dist = np.sqrt(np.power(1. - recall, 2) + np.power(1. - precision, 2))  # Distance from (1, 1)
+    return thresholds[np.argmin(eucl_dist)]
+    # f1 = 2. * (precision * recall) / (precision + recall)
+    # return thresholds[np.argmax(f1)]
+    # return thresholds[np.argmin(x)]
