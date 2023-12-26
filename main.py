f901a9648cd31fda321c62789fe35c76a60a357b
livanosg <livanos.g@gmail.com>
2023-12-27 01:47:34 +0200

    write main

--- main.py
+++ main.py
@@ -1,35 +1,84 @@
 import os
 from datetime import datetime
 
+import keras.models
 import numpy as np
 
-from config import VALIDATION_DIR, TEST_DIR
-from local_utils import trial_dirs
-from training import training
-from validation import validate_model, calculate_threshold
+from config import TRIALS_DIR
+from dataset import get_dataset, split_inputs_labels
+from metrics import plot_history, plot_pr_cure, print_metrics, plot_confusion_matrix
+from model import calculate_threshold
+from training import model_training
 
 if __name__ == '__main__':
-    trial_id = str(datetime.now().strftime("%Y%m%d%H%M%S"))
-    trial_dir, model_path, history_path, threshold_path = trial_dirs(trial_id)
-    model = training(model_path=model_path, history_path=history_path)
-    validate_model(model=model,
-                   data_dir=VALIDATION_DIR,
-                   threshold_path=threshold_path,
-                   history_path=history_path, name="validation")
-    validate_model(model=model,
-                   data_dir=TEST_DIR,
-                   history_path=history_path,
-                   threshold_path=threshold_path, name="test",)
 
-    x_input = data.map(lambda images, _: images, num_parallel_calls=tf.data.AUTOTUNE)
-    y_output = model.predict(x_input)
-    y_true = data.map(lambda _, labels: labels, num_parallel_calls=tf.data.AUTOTUNE).unbatch()
-    y_true = np.asarray([label for label in y_true.as_numpy_iterator()])
+    # trial_id = "latest"
+    # mode = "validation"
+    trial_id = None
+    mode = "train"
+    if trial_id is None:
+        trial_id = str(datetime.now().strftime("%Y%m%d%H%M%S"))
+    elif trial_id == "latest":
+        trial_id = sorted(os.listdir(TRIALS_DIR), key=lambda x: datetime.strptime(x, "%Y%m%d%H%M%S"))[-1]
+        print("latest_trial_id", trial_id)
 
-    y_output = y_output.squeeze()
-    y_true = y_true.squeeze()
+    trial_dir = os.path.join(TRIALS_DIR, str(trial_id))
+    model_path = os.path.join(str(trial_dir), "model.keras")
+    history_path = os.path.join(str(trial_dir), "history.csv")
+    threshold_path = os.path.join(str(trial_dir), "threshold.npy")
+    backup_dir = os.path.join(str(trial_dir), "backup")
+    threshold = None
+    if mode == "train":
+        model = model_training(model_path=model_path,
+                               backup_dir=backup_dir,
+                               history_path=history_path)
+        keras.models.save_model(model_path)
 
-    if os.path.isfile(threshold_path):
+    if mode in ("train", "validation"):
+        model = keras.models.load_model(model_path)
+
+        x_val, y_val_true = split_inputs_labels(get_dataset(dataset="validation"))
+        y_val_true = np.concatenate(list(y_val_true.as_numpy_iterator()))
+
+        y_val_pred_prob = model.predict(x_val).squeeze()
+
+        y_true = y_val_true[:, 1]
+        y_pred_prob = y_val_pred_prob[:, 1]
+        threshold = calculate_threshold(y_true, y_pred_prob)
+        np.save(threshold_path, threshold)
+        y_pred = np.greater_equal(y_pred_prob, threshold).squeeze().astype(int)
+        plot_history(history_path)
+        plot_pr_cure(y_true, y_pred_prob)
+
+        print_metrics(y_true, y_pred)
+        plot_confusion_matrix(y_true, y_pred)
+    if mode in ("train", "test"):
+        model = keras.models.load_model(model_path)
+
+        x_val, y_val_true = split_inputs_labels(get_dataset(dataset="validation"))
+        y_val_true = np.concatenate(list(y_val_true.as_numpy_iterator()))
+
+        y_val_pred_prob = model.predict(x_val).squeeze()
+
+        y_true = y_val_true[:, 1]
+        y_pred_prob = y_val_pred_prob[:, 1]
         threshold = np.load(threshold_path)
-    else:
-        threshold = calculate_threshold(y_true, y_output, threshold_path)
+        y_pred = np.greater_equal(y_pred_prob, threshold).squeeze().astype(int)
+        plot_pr_cure(y_true, y_pred_prob)
+
+        print_metrics(y_true, y_pred)
+        plot_confusion_matrix(y_true, y_pred)
+
+    elif mode == "predict":
+        model = keras.models.load_model(model_path)
+
+        x_val, y_val_true = split_inputs_labels(get_dataset(dataset="validation"))
+        y_val_true = np.concatenate(list(y_val_true.as_numpy_iterator()))
+
+        y_val_pred_prob = model.predict(x_val).squeeze()
+
+        y_true = y_val_true[:, 1]
+        y_pred_prob = y_val_pred_prob[:, 1]
+        threshold = np.load(threshold_path)
+        y_pred = np.greater_equal(y_pred_prob, threshold).squeeze().astype(int)
+        print(y_pred)
