68591094c5364dc7c95294540a7b5f6fe5b46bfe
livanosg <livanos.g@gmail.com>
2023-12-27 01:46:19 +0200

    fix config.py

--- config.py
+++ config.py
@@ -1,22 +1,31 @@
 import itertools
 import os
+from datetime import datetime
 
+URL_DATA_FILE = "https://repository.detectionnow.com/content/rgb/denim_elastane.mp4"
+FILE_NAME = os.path.basename(URL_DATA_FILE)
 
-ANOMALY_INDICES = list(itertools.chain(range(45, 538), range(799, 1266), range(1452, 2311),
-                                       range(2495, 2931), range(2954, 3417), range(4092, 4817)))
-CLASS_NAMES = ["normal", "anomaly"]
-
+TRIAL_ID = str(datetime.now().strftime("%Y%m%d%H%M%S"))
 ROOT_DIR = os.path.dirname(__file__)
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-TRIALS_DIR = os.path.join(ROOT_DIR, "trials")
-
-DATA_FILE = os.path.join(DATA_DIR, "data.csv")
 RAW_DIR = os.path.join(DATA_DIR, "raw")
+TRIALS_DIR = os.path.join(ROOT_DIR, "trials")
 IMAGES_DIR = os.path.join(DATA_DIR, "images")
 TRAIN_DIR = os.path.join(DATA_DIR, "train")
 VALIDATION_DIR = os.path.join(DATA_DIR, "validation")
 TEST_DIR = os.path.join(DATA_DIR, "test")
 
-MODEL_NAME = "model.keras"
-HISTORY_NAME = "history.csv"
-THRESHOLD_NAME = "threshold.npy"
+CSV_FILE = os.path.join(DATA_DIR, "data.csv")
+VIDEO_FILE = os.path.join(RAW_DIR, os.path.basename(URL_DATA_FILE))
+CLASS_NAMES = ["normal", "anomaly"]
+ANOMALY_INDICES = list(itertools.chain(range(45, 538), range(799, 1266), range(1452, 2311),
+                                       range(2495, 2931), range(2954, 3417), range(4092, 4817)))
+
+SEED = 1312
+BATCH_SIZE = 64
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
+CHANNELS = 3
+INPUT_SHAPE = (IMG_HEIGHT, IMG_WIDTH, CHANNELS)
+EPOCHS = 400
+LEARNING_RATE = 1e-3
