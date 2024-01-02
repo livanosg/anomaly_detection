0503a2d3d3566c5872255ccb986ec15b99760c4a
livanosg <livanos.g@gmail.com>
2024-01-02 13:27:50 +0200

    change supervised model

--- model.py
+++ model.py
@@ -1,21 +1,19 @@
-import os
-
-import numpy as np
 from keras import Sequential, Input
-from keras.layers import Dense, SeparableConv2D, Flatten, MaxPooling2D, Convolution2DTranspose
-from sklearn.metrics import precision_recall_curve
+from keras.layers import SeparableConv2D, MaxPooling2D, Convolution2DTranspose, GlobalAveragePooling2D
 
 
 def supervised_anomaly_detector(input_shape):
     return Sequential([
         Input(shape=input_shape),
-        SeparableConv2D(16, 3, depth_multiplier=3, padding='valid', activation='relu'),
-        SeparableConv2D(8, 5, depth_multiplier=2, padding='valid', activation='relu'),
+        SeparableConv2D(64, 3, depth_multiplier=3, padding='valid', activation='relu'),
+        SeparableConv2D(128, 5, depth_multiplier=2, padding='valid', activation='relu'),
         MaxPooling2D(),
-        SeparableConv2D(8, 3, depth_multiplier=1, padding='same', activation='relu'),
-        Flatten(),
-        Dense(32, activation='relu'),
-        Dense(1, activation="sigmoid")
+        SeparableConv2D(128, 5, depth_multiplier=2, padding='valid', activation='relu'),
+        MaxPooling2D(),
+        SeparableConv2D(64, 3, depth_multiplier=1, padding='same', activation='relu'),
+        SeparableConv2D(32, 3, depth_multiplier=1, padding='same', activation='relu'),
+        SeparableConv2D(1, 3, depth_multiplier=1, padding='same', activation='sigmoid'),
+        GlobalAveragePooling2D()
     ])
 
 
@@ -36,12 +34,3 @@ def unsupervised_anomaly_detector(input_shape):
          Convolution2DTranspose(128, 7, (2, 2), padding="same", activation="relu"),  # (224, 224)
          SeparableConv2D(3, 3, depth_multiplier=3, padding="same", activation="sigmoid")  # (224, 224)
          ])
-
-
-def pr_threshold(y_true, probas_pred, conf, save=True):
-    precision, recall, thresholds = precision_recall_curve(y_true=y_true, probas_pred=probas_pred)
-    euclidean_distance = np.sqrt(np.power(1. - recall, 2) + np.power(1. - precision, 2))  # Distance from (1, 1)
-    threshold = thresholds[np.argmin(euclidean_distance)]
-    if save:
-        np.save(os.path.join(conf.model_dir, "threshold.npy"), threshold)
-    return threshold
