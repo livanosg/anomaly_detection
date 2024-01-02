8e1e5b493b0e5a466a94866b45130210889be2d0
livanosg <livanos.g@gmail.com>
2024-01-02 13:30:11 +0200

    isolate set up script

--- setup_data.py
+++ setup_data.py
@@ -9,8 +9,20 @@ import pandas as pd
 import requests
 from tqdm import tqdm
 
-from config import RAW_DIR, VIDEO_URL, VIDEO_FILE, IMAGES_DIR, TRAIN_DIR, VALIDATION_DIR, CLASS_NAMES, \
-                    ANOMALIES, ANOMALIES_INDICES
+from project_manage import ROOT_DIR, CLASS_NAMES
+
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+RAW_DIR = os.path.join(DATA_DIR, "raw")
+
+anomalies = [range(45, 538), range(799, 1266), range(1452, 2311),
+             range(2495, 2931), range(2954, 3417), range(4092, 4817)]
+anomalies_idx = list(itertools.chain(*anomalies))
+
+
+DATASETS = {"train": os.path.join(DATA_DIR, "train"),
+            "validation": os.path.join(DATA_DIR, "validation"),
+            "all": os.path.join(DATA_DIR, "images")
+            }
 
 
 def download_data(url, save_path):
@@ -45,16 +57,16 @@ def download_data(url, save_path):
     print(f"File {file_name} downloaded!")
 
 
-def extract_images(video_path, dataset_dir):
+def extract_images(from_video, to_dir):
     """
     Extract frames from a video and save them as images.
     """
     print("Extract video to images...")
-    video = cv2.VideoCapture(video_path)
+    video = cv2.VideoCapture(from_video)
     processes = []
-    image_base_name = os.path.splitext(os.path.basename(video_path))[0]
-    shutil.rmtree(dataset_dir)
-    [os.makedirs(os.path.join(dataset_dir, x), exist_ok=True) for x in CLASS_NAMES]
+    image_base_name = os.path.splitext(os.path.basename(from_video))[0]
+    shutil.rmtree(to_dir)
+    [os.makedirs(os.path.join(to_dir, class_name), exist_ok=True) for class_name in CLASS_NAMES]
 
     while video.isOpened():
         frame_index = int(video.get(cv2.CAP_PROP_POS_FRAMES))
@@ -63,14 +75,14 @@ def extract_images(video_path, dataset_dir):
         if not ret:
             break
         image_name = image_base_name + formatted_index + ".png"
-        save_path = os.path.join(dataset_dir, CLASS_NAMES[int(frame_index in ANOMALIES_INDICES)], image_name)
+        save_path = os.path.join(to_dir, CLASS_NAMES[int(frame_index in anomalies_idx)], image_name)
         process = Process(target=cv2.imwrite, args=(save_path, frame))
         process.start()
         processes.append(process)
     for process in processes:
         process.join()
 
-    create_csv(dataset_dir=dataset_dir)
+    create_csv(dataset_dir=to_dir)
     print("Extract video to images done!")
 
 
@@ -79,7 +91,7 @@ def _get_index(image_path):
 
 
 def _get_label(image_path):
-    return int(_get_index(image_path) in ANOMALIES_INDICES)
+    return int(_get_index(image_path) in anomalies_idx)
 
 
 def create_csv(dataset_dir):
@@ -91,7 +103,8 @@ def create_csv(dataset_dir):
 
     The resulting CSV file is saved in the DATA_DIR directory with the name "data.csv".
     """
-    data = pd.concat([pd.DataFrame(data={"images": os.listdir(os.path.join(dataset_dir, str(_class)))}) for _class in CLASS_NAMES])
+    data = pd.concat(
+        [pd.DataFrame(data={"images": os.listdir(os.path.join(dataset_dir, str(_class)))}) for _class in CLASS_NAMES])
     data["labels"] = data["images"].map(_get_label)
     data.sort_values("images", key=lambda x: x.map(_get_index), inplace=True)
     data.to_csv(os.path.join(dataset_dir, f"{os.path.basename(dataset_dir)}_data.csv"), index=False)
@@ -104,17 +117,18 @@ def split_train_val(val_size=0.2):
     Args:
         val_size (float): Fraction of the dataset to use for validation.
     """
-    df = pd.read_csv(os.path.join(IMAGES_DIR, f"{os.path.basename(IMAGES_DIR)}_data.csv"))
-    train_data = ANOMALIES[:int(len(ANOMALIES) * (1 - val_size))]
+
+    df = pd.read_csv(os.path.join(DATASETS["all"], f"{os.path.basename(DATASETS['all'])}_data.csv"))
+    train_data = anomalies[:int(len(anomalies) * (1 - val_size))]
     train_idx = list(itertools.chain(*train_data))[-1]
     train_df = df.iloc[df.index <= train_idx]
     val_df = df.iloc[df.index > train_idx]
-    for data_dir, df in [[TRAIN_DIR, train_df], [VALIDATION_DIR, val_df]]:
+    for data_dir, df in [[DATASETS["train"], train_df], [DATASETS["validation"], val_df]]:
         if os.path.isdir(data_dir):
             shutil.rmtree(data_dir)
         for image_name in df["images"].values:
-            _class = CLASS_NAMES[int(_get_index(image_name) in ANOMALIES_INDICES)]
-            from_dir = os.path.join(IMAGES_DIR, _class, image_name)
+            _class = CLASS_NAMES[int(_get_index(image_name) in anomalies_idx)]
+            from_dir = os.path.join(DATASETS["all"], _class, image_name)
             to_dir = os.path.join(data_dir, _class, image_name)
             os.makedirs(os.path.dirname(to_dir), exist_ok=True)
             shutil.copyfile(from_dir, to_dir)
@@ -124,8 +138,8 @@ def split_train_val(val_size=0.2):
 if __name__ == '__main__':
     video_url = "https://repository.detectionnow.com/content/rgb/denim_elastane.mp4"
     video_file = os.path.basename(video_url)
-    video_local_path = os.path.join(RAW_DIR, video_file)
-    [os.makedirs(_dir, exist_ok=True) for _dir in [RAW_DIR, IMAGES_DIR]]
-    download_data(url=VIDEO_URL, save_path=VIDEO_FILE)
-    # extract_images(video_path=VIDEO_FILE, dataset_dir=IMAGES_DIR)
-    split_train_val(0.1)
+    video_path = os.path.join(RAW_DIR, video_file)
+    [os.makedirs(_dir, exist_ok=True) for _dir in [RAW_DIR, DATASETS["all"]]]
+    download_data(url=video_url, save_path=video_path)
+    extract_images(from_video=video_path, to_dir=DATASETS["all"])
+    split_train_val(0.2)
