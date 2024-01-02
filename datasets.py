5eaf51870d42d69b13b6555584a8e2312eef3214
livanosg <livanos.g@gmail.com>
2024-01-02 13:26:45 +0200

    add data augmentation

--- datasets.py
+++ datasets.py
@@ -1,25 +1,38 @@
 import keras
 import tensorflow as tf
-from config import SEED, TRAIN_DIR, VALIDATION_DIR, TEST_DIR, IMAGES_DIR
+from icecream import ic
+
+from setup_data import DATASETS
 
 
-def get_dataset(mode, shuffle, conf):
-    datasets = {"train": TRAIN_DIR,
-                "validation": VALIDATION_DIR,
-                "test": TEST_DIR,
-                "all": IMAGES_DIR
-                }
-    dataset = keras.utils.image_dataset_from_directory(directory=datasets[mode],
-                                                       labels="inferred",
-                                                       label_mode="int",
-                                                       class_names=conf.class_names,
-                                                       image_size=conf.input_shape[:-1],
-                                                       batch_size=conf.batch_size,
-                                                       shuffle=shuffle,
-                                                       seed=SEED)
+def image_data_augmentation(image, label, seed):
+    seeds = (seed, seed)
+    # Data augmentation
+    image = tf.image.stateless_random_flip_left_right(image, seed=seeds)
+    image = tf.image.stateless_random_flip_up_down(image, seed=seeds)
+    image = tf.image.stateless_random_brightness(image, max_delta=0.2, seed=seeds)
+    image = tf.image.stateless_random_contrast(image, lower=0.5, upper=1.5, seed=seeds)
+    image = tf.image.stateless_random_saturation(image, lower=0.5, upper=1.5, seed=seeds)
+    image = tf.image.stateless_random_hue(image, max_delta=0.2, seed=seeds)
+    return image, label
 
-    dataset = dataset.map(lambda image, label: (image / 255., label), num_parallel_calls=tf.data.AUTOTUNE, name="Normalize")
-    return dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+
+def get_dataset(dataset, conf, shuffle=False, augm=False,):
+    ds = keras.utils.image_dataset_from_directory(directory=DATASETS[dataset],
+                                                  labels="inferred",
+                                                  label_mode="int",
+                                                  class_names=conf.class_names,
+                                                  image_size=conf.input_shape[:-1],
+                                                  batch_size=conf.batch_size,
+                                                  shuffle=shuffle,
+                                                  seed=conf.seed)
+
+    if augm:
+        ds = ds.map(lambda image, label: image_data_augmentation(image, label, conf.seed),
+                    num_parallel_calls=tf.data.AUTOTUNE, name="image_augm")
+    ds = ds.map(lambda image, label: (tf.cast(image, tf.float32) / 255., label),
+                num_parallel_calls=tf.data.AUTOTUNE, name="normalize")
+    return ds.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 
 def split_inputs_labels(dataset):
